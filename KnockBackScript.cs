5d75fb58eeb23dca6ecda9dbbe9f95724e719cd7
jojitoman <joaquinmuleirobeltran@gmail.com>
2017-05-05 11:39:59 -0300

    Add files via upload

---
+++ KnockBackScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KnockBackScript : MonoBehaviour {
+
+    private Rigidbody2D rb2d;
+
+	// Use this for initialization
+	void Start () {
+        rb2d = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    public IEnumerator Knockback(float knockDur, float knockPwr, Vector3 knockbackDir)
+    {
+        float timer = 0;
+        while (knockDur > timer)
+        {
+            timer += Time.deltaTime;
+            rb2d.AddForce(new Vector3(knockbackDir.x * -100, knockbackDir.y * knockPwr, transform.position.z));
+        }
+        yield return 0;
+    }
+}
---
+++ Spikes.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spikes : MonoBehaviour {
+
+    private PlayerBehaviour player;
+    private KnockBackScript knock;
+    private killPlayer dmgplayer;
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerBehaviour>();
+
+    }
+    void OnTriggerEnter2D (Collider2D col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            if (!dmgplayer.invincible)
+            {
+                if (col.name == "Player")
+                {
+                    player.Damage(1);
+                    StartCoroutine(knock.Knockback(0.02f, 350, player.transform.position));
+                    dmgplayer.invincible = true;
+                    Invoke("dmgplayer.resetInvulnerability", 2);
+                    
+                }
+
+            }
+        }
+    }
+    }
+}
