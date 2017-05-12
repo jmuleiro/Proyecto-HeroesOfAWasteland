ab521aaef3f4df04e9240ab0aa776d7be8235f1a
jojitoman <joaquinmuleirobeltran@gmail.com>
2017-05-12 11:46:52 -0300

    Add files via upload

---
+++ WorldGen/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+    public BoardManager boardScript;
+
+
+    private int level = 3;
+
+    void Awake()
+    {
+        boardScript = GetComponent<BoardManager>();
+        InitGame();
+    }
+    void InitGame()
+    {
+        
+    }
+
+
+	void Start () {
+	
+	}
+	
+	void Update () {
+	
+	}
+}
---
+++ WorldGen/IntRange.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Para que se muestre en el inspector
+public class IntRange : MonoBehaviour {
+    public int m_Min;
+    public int m_Max;
+
+    public IntRange(int min, int max)
+    {
+        m_Min = min;
+        m_Max = max;
+    }
+    public int Random
+    {
+        get { return UnityEngine.Random.Range(m_Min, m_Max); }
+    }
+}
